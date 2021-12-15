2c254c4f9a192fd2cd40e92f3026ffe1eb9e0c71
James Gibson <ja.gibson@hotmail.co.uk>
2021-12-15 20:46:20 +0000

    day 15 done

--- src/Jag.AdventOfCode/Y2021/Day15/Solver.cs
+++ src/Jag.AdventOfCode/Y2021/Day15/Solver.cs
@@ -19,52 +19,92 @@ namespace Jag.AdventOfCode.Y2021.Day15
             };
         public string SolvePart1(string input)
         {
-            return FindShortestPath(ParseInput(input)).ToString();
+            return FindShortestPath(ParseInput1(input)).ToString();
         }
 
         public string SolvePart2(string input)
         {
-            throw new NotImplementedException();
+            return FindShortestPath(ParseInput2(input)).ToString();
         }
 
         private int FindShortestPath(int[][] grid)
         {
-            var priorityQueue = new SimplePriorityQueue<((int row, int col) current, HashSet<(int row, int col)> visited), int>();
-            
-            priorityQueue.Enqueue(((0, 0), new HashSet<(int, int)> { (0, 0) }), 0);
+            var distances = Enumerable.Repeat(0, grid.Length)
+                .Select(_ => Enumerable.Repeat(int.MaxValue, grid[0].Length).ToArray())
+                .ToArray();
+
+            var unvisited = new HashSet<(int row, int col)>();
+            for (int row = 0; row < grid.Length; row++)
+                for (int col = 0; col < grid[0].Length; col++)
+                    unvisited.Add((row, col));
+
+            distances[0][0] = 0;
 
             int lastRow = grid.Length - 1;
             int lastCol = grid[0].Length - 1;
 
+            var priorityQueue = new SimplePriorityQueue<(int row, int col), int>();
+            priorityQueue.Enqueue((0, 0), 0);
+            
+            int dequeues = 0;
             while (priorityQueue.Count != 0)
             {
-                var ((row, col), currentPath) = priorityQueue.Dequeue();
+                var (row, col) = priorityQueue.Dequeue();
+                dequeues++;
+                if (!unvisited.Contains((row, col)))
+                    continue;
+
+                var currentScore = distances[row][col];
+
+                if ((row, col) == (lastRow, lastCol))
+                {
+                    return currentScore;
+                }
                 var neighbours = RelativeNeighbours.Select(n => (row: n.row + row, col: n.col + col))
-                    .Where(x => x.col >= 0 && x.row >= 0 && x.col <= lastCol && x.row <= lastRow && !currentPath.Contains((x.row, x.col)))
+                    .Where(x => x.col >= 0 && x.row >= 0 && x.col <= lastCol && x.row <= lastRow && unvisited.Contains((x.row, x.col)))
                     .OrderBy(x => grid[x.row][x.col]);
+
                 foreach (var (nrow, ncol) in neighbours)
                 {
-                    var newCurrentPath = new HashSet<(int row, int col)>(currentPath);
-                    newCurrentPath.Add((nrow, ncol));
-                    var score = newCurrentPath.Sum(x => grid[x.row][x.col]);
-                    if ((ncol, nrow) == (lastCol, lastRow))
+                    var neighbourScore = currentScore + grid[nrow][ncol];
+                    if (distances[nrow][ncol] > neighbourScore)
                     {
-                        return score - grid[0][0];
+                        distances[nrow][ncol] = neighbourScore;
                     }
-                    priorityQueue.Enqueue(((nrow, ncol), new HashSet<(int row, int col)>(newCurrentPath)), score);
+                    priorityQueue.Enqueue((nrow, ncol), neighbourScore);
                 }
+                unvisited.Remove((row, col));
             }
-            
             throw new Exception();
         }
 
-        private int[][] ParseInput(string input)
+        private int[][] ParseInput1(string input)
         {
             return input.Split(Environment.NewLine, options: StringSplitOptions.RemoveEmptyEntries)
                 .Select(s => s.ToCharArray()
                     .Select(c => int.Parse(c.ToString()))
                     .ToArray())
                 .ToArray();
+        }
+        private int[][] ParseInput2(string input)
+        {
+            var grid1 = ParseInput1(input);
+            var grid2 = Enumerable.Repeat(0, grid1.Length * 5)
+                .Select(_ => Enumerable.Repeat(0, grid1[0].Length * 5).ToArray())
+                .ToArray();
+            // var lastRow = grid1.Length - 1;
+            // var lastCol = 
+            for (int row = 0; row < grid2.Length; row++)
+            {
+                for (int col = 0; col < grid2[0].Length; col++)
+                {
+                    var offset = (row / grid1.Length) + (col / grid1[0].Length);
+                    var baseRisk = grid1[row % grid1.Length][col % grid1.Length];
+                    grid2[row][col] = offset == 0 ? baseRisk : ((baseRisk - 1 + offset) % 9) + 1;
+                }
+            }
+
+            return grid2;
         }
     }
 }
